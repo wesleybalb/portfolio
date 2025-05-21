95b39c57088573878a1d6b9aa60e406b38b38f8d
wesleybalb <wesleybalb@gmail.com>
2025-05-21 20:12:43 -0300

    Convert project from TypeScript to JavaScript

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,11 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -25,4 +23,4 @@ export default tseslint.config(
       ],
     },
   }
-);
+];
